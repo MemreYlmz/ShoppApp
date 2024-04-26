7732f89d64c6af7121a755be9b0de6349b9b1833
Mehmet_Emre Yılmaz <mehmetemreyilmaz199991@gmail.com>
2024-04-26 19:13:17 +0300

    buyProduct design modification

--- src/components/ShoppingCard/BuyProduct.js
+++ src/components/ShoppingCard/BuyProduct.js
@@ -8,23 +8,23 @@ function BuyProduct({ _id,productName,productPrice,productImg,quantity,productCa
     const dispatch = useDispatch()
     
   return (
-    <> {quantity>0 ? <div className='buy-product-firstDiv' >
-    <img className='buy-product-img'  src={productImg} alt="" />
-    <div >
-      <h5>{productName}</h5>
-      <h5>{productPrice}TL</h5>
-      <div>
-        <button onClick={()=> dispatch(increase(_id))} >
-          <BsChevronUp />
-        </button>
-        <p className="cartQuantity"></p>
-        <button onClick={()=>dispatch(decrease(_id))} >
-          <BsChevronDown />
-        </button>
-      </div>
-      <button  onClick={()=>{dispatch(removeItem(_id))}}>Sil</button>
+    <> {quantity>0 ?   <><div className='buy-product-firstDiv'>
+    <img className='buy-product-img' src={productImg} alt="" />
+    <div className="product-details">
+        <div className="product-info">
+            <h5>{productName}</h5>
+            <h5>{productPrice}TL</h5>
+        </div>
+        <div className="quantity-controls">
+            <div className="button-container">
+                <BsChevronUp onClick={() => dispatch(increase(_id))} />
+                <p className="cartQuantity">{quantity}</p>
+                <BsChevronDown  onClick={() => dispatch(decrease(_id))} />
+            </div>
+            <button className='removebtn' onClick={() => { dispatch(removeItem(_id)) }}>Sil</button>
+        </div>
     </div>
-  </div> : null}
+</div> <hr></hr></> : null}
     
 
     </>
